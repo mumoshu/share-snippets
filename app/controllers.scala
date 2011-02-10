c386cc3cbc7e11a333cbac3d966e875146674629
KUOKA Yusuke <ykuoka@gmail.com>
2011-02-10 04:57:55 -0800

    make a post

--- app/controllers.scala
+++ app/controllers.scala
@@ -12,18 +12,21 @@ object Application extends Controller {
 
 object Posts extends Controller {
 
-  def index {
-    val post = new Post(new Lang("lang"), new Topic("topic"), "code")
-    Template
+  def index = {
+    val lan = new Lang("lang")
+    lan.save
+    val topic = new Topic("topic")
+    topic.save
+    Template(lan, topic)
   }
 
-  def create(lang: Lang, topic: Topic, code: String) {
+  def create(lang: Lang, topic: Topic, code: String) = {
     val post = new Post(lang, topic, code)
     post.validateAndSave
-    show(post)
+    Action(show(post))
   }
 
-  def show(post: Post) = Template
+  def show(post: Post) = Template(post)
 
   def recent = Template
 }
