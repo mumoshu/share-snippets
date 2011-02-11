b11e0462f3ceda3e92516e72aa1859563cdac923
KUOKA Yusuke <ykuoka@gmail.com>
2011-02-11 02:02:07 -0800

    an example of binding an instance of Model to the method argument.

--- app/controllers.scala
+++ app/controllers.scala
@@ -10,6 +10,15 @@ object Application extends Controller {
   def index = Posts.index
 }
 
+object Usages extends Controller {
+  /**
+   * Invoked when requested /usages/bind_post?post.id=<id>.<br />
+   * <br />
+   * post is binded with <code>Post.findById(id).getOrElse({ val post = new Post; post.id = id; post })</code>
+   */
+  def bind_post(post: Post) = if (post != null) "id=%d, code=%s".format(post.id, post.code)
+}
+
 object Posts extends Controller {
 
   def index = {
