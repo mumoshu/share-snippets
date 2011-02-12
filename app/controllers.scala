ea03bb54e0ff03ca4d5f067fb417663b61b0cd3f
KUOKA Yusuke <ykuoka@gmail.com>
2011-02-12 00:42:33 -0800

    login & tagging support (may not compile)

--- app/controllers.scala
+++ app/controllers.scala
@@ -8,6 +8,14 @@ import models._
 object Application extends Controller {
     
   def index = Posts.index
+
+  def login = Template
+
+  def authenticate(userId: Long) = {
+    session.put("user.id", userId)
+    renderArgs += "user" -> Users.findById(userId).getOrNotFound
+    Action(index)
+  }
 }
 
 object Usages extends Controller {
@@ -19,7 +27,7 @@ object Usages extends Controller {
   def bind_post(post: Post) = if (post != null) "id=%d, code=%s".format(post.id, post.code)
 }
 
-object Posts extends Controller {
+object Posts extends Controller with Secured {
 
   def index = {
     val lan = new Lang("lang")
@@ -37,7 +45,22 @@ object Posts extends Controller {
 
   def show(post: Post) = Template(post)
 
+  def tag(post: Post, tagName: String) = {
+    val user = renderArgs.get("user")
+
+    user.tagPost(post, tagName)
+  }
+
   def recent = Template
+}
+
+trait Secure extends Controller {
+  @Before def ensureLogin = {
+    session("user.id") map(_.toLong) match {
+      case Some(userId: Long) => renderArgs += "user" -> User.findById(userId).getOrNotFound; Continue
+      case None => Action(Application.login)
+    }
+  }
 }
 
 package admin {
--- app/models.scala
+++ app/models.scala
@@ -3,6 +3,8 @@ package models
 import play.db.jpa._
 import play.data.validation._
 
+import java.util.{List => JList}
+
 @Entity
 case class Post(
   @ManyToOne
@@ -10,13 +12,15 @@ case class Post(
   var lang: Lang,
 
   @ManyToOne
-  @Required
   var topic: Topic,
 
   @Required
   @Lob
   var code: String
+
 ) extends Model {
+
+  def taggings = Tagging.find("byPost", this)
 }
 
 object Post extends QueryOn[Post]
@@ -38,3 +42,53 @@ case class Topic(
 }
 
 object Topic extends QueryOn[Topic]
+
+@Entity
+case class Tag(
+  @Required
+  var name: String
+) extends Model
+
+object Tag extends QueryOn[Tag] {
+  def findByNameOrCreate(name: String) = {
+    find("byName", name).getOrElse(new Tag(name).save)
+  }
+}
+
+// Model, QueryOnのメソッド内でfind/createするのは一回だけにする。
+// ２回も３回もDB操作している場合は、そのメソッドに役割が集中しすぎているのではないかという仮説から。
+
+@Entity
+case class User(
+  @Required
+  var name: String
+) extends Model {
+
+  def tagPost(post: Post, tagName: String) = {
+    val tag = Tag.findByNameOrCreate(tagName)
+
+    Tagging.createIfNotFound(user, post, tag)
+  }
+}
+
+object User extends QueryOn[User]
+
+@Entity
+case class Tagging(
+  @Required
+  var user: User,
+
+  @Required
+  var post: Post,
+
+  @Required
+  var tag: Tag
+)
+
+object Tagging extends QueryOn[Tagging] {
+  def createIfNotFound(user: User, post: Post, tag: Tag) = {
+    find("byUserAndPostAndTag", user, post, tag).getOrElse(
+      new Tagging(user, post, tag).save
+    )
+  }
+}
